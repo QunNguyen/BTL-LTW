c4a7b7481cc31b1ad470591b0655fa9d152cce13
DuongTuKien <kien.dt@tinasoft.vn>
2022-12-19 14:29:47 +0700

    up

--- server/database.js
+++ server/database.js
@@ -18,7 +18,6 @@ app.post('/register', (req, res) => {
     const account = req.body.account;
     const password = req.body.password;
     const email = req.body.email;
-    console.log(account, password, email)
     pool.query("INSERT INTO user (`account`, `password`, `email`) VALUES (?,?,?);",
         // eslint-disable-next-line no-undef
         [account, password, email],
@@ -40,26 +39,31 @@ app.post('/register', (req, res) => {
 app.post('/login', (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
-    console.log(email, password)
-    pool.query("select * from user Where account = ? and password= ?",
+    pool.query("select * from user",
         // eslint-disable-next-line no-undef
-        [email, password],
+        [],
         (err, reslut) => {
+            let check=false;
             if (err) {
                 console.log(err);
             }
             if (reslut) {
-                console.log("dang nhap thanh cong")
-                res.send(reslut);
+                // console.log(reslut)
+                for (let index = 0; index < reslut.length; index++) {
+                    const element = reslut[index];
+                    if (element.email === email && element.password === password) {
+                        check=true;
+                        res.send(true);
+                        break;
+                    }
+                }
             }
-            else {
-                console.log('dang nhap khong thanh cong');
-            }
+            if(check==false) res.send(false);
         });
 })
 
 app.post('/data', (req, res) => {
-    pool.query("select * from user",
+    pool.query("SELECT email FROM ltw.user;",
         // eslint-disable-next-line no-undef
         [],
         (err, reslut) => {
@@ -95,6 +99,22 @@ app.post('/main/databook', (req, res) => {
 })
 
 
+app.post('/main/databook/name', (req, res) => {
+    pool.query("SELECT name FROM ltw.book",
+        // eslint-disable-next-line no-undef
+        [],
+        (err, reslut) => {
+            if (err) {
+                console.log(err);
+            }
+            if (reslut) {
+                res.send(reslut)
+            }
+        });
+}
+)
+
+
 app.post('/main/deleteData', (req, res) => {
     const idbook = req.body.idbook;
     pool.query("DELETE FROM book WHERE idbook = ?",
@@ -123,10 +143,9 @@ app.post('/main/Update', (req, res) => {
     const image = req.body.image;
     const type = req.body.type;
     const note = req.body.note;
-    console.log(req.body)
     pool.query("UPDATE `ltw`.`book` SET `name` = ?, `author` =?, `date` = ?, `numberbook` = ?, `image` = ?, `type` = ?, `note` = ? WHERE idbook = ?",
         // eslint-disable-next-line no-undef
-        [name,author,date,numberbook,image,type,note,idbook],
+        [name, author, date, numberbook, image, type, note, idbook],
         (err, reslut) => {
             if (err) {
                 console.log(err);
@@ -142,15 +161,35 @@ app.post('/main/Update', (req, res) => {
 })
 
 
+app.post('/main/NewBook', (req, res) => {
+    const name = req.body.name;
+    const author = req.body.author;
+    const date = req.body.date;
+    const numberbook = req.body.numberbook;
+    const image = req.body.image;
+    const type = req.body.type;
+    const note = req.body.note;
+    pool.query("INSERT INTO book ( `name`, `author`, `date`, `numberbook`, `image`, `type`, `note`) VALUES ( ?, ?, ?, ?, ?, ?,?);",
+        // eslint-disable-next-line no-undef
+        [name, author, date, numberbook, image, type, note],
+        (err, reslut) => {
+            if (err) {
+                console.log(err);
+            }
+            if (reslut) {
+                console.log("Tao moi thanh cong")
+                res.send(reslut);
+            }
+            else {
+                console.log('Tao moi khong thanh cong');
+            }
+        });
+})
+
+
 app.listen(3001, () => {
     console.log("running")
 })
 
-// pool.query('SELECT * FROM product',(err,res)=>{
-//     if(err)
-//     {
-//         return console.log(err);
-//     }
-//     return console.log(res)
-// })
+
 
